2c63d4ce7cdee0c096841774233e39be27ecfeab
yagyanssh <yagyansh9999@gmail.com>
2024-03-07 02:00:03 +0530

    Added profile page

--- README.md
+++
@@ -1,4 +0,0 @@
-<h1>Social Media Application</h1>
-<p>Its a Social Media Application build using Djngo and Python.</p>
-<p>This application has integrated feaures like URL Routing
-Template Setup, Static Files, Profile Model, Signup, Signin and Logout, Account Settings, Uploading Post, Post Feed, Like Post, Profile Page, Follow and Unfollow User, Post Feed Updated, Download Images, Search User, User Suggestions</p>
--- dashboard/views.py
+++ dashboard/views.py
@@ -49,7 +49,20 @@ def like_post(request):
         post.no_of_likes = post.no_of_likes - 1
         post.save()
         return redirect('/')
-        
+       
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_post = Post.objects.filter(user=pk)
+    user_post_length = len(user_post)
+    
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_post': user_post,
+        'user_post_length': user_post_length,
+    }
+    return render (request, 'profile.html', context)
 
 @login_required(login_url='signin')
 def settings(request):
